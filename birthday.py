fae12a1b4be9e16b22af47a181a1f1aa53e13945
Jason Ward <jason55ward@gmail.com>
2022-04-01 00:31:52 +0200

    reading filename from cli and reading json file

--- birthday.py
+++ birthday.py
@@ -13,23 +13,43 @@ DEFAULT_JSON=[
 def get_birthday_list(data, custom_today_date=None):
     result = []
     for row in data:
-        pass
+        print(row)
     return result
 
 def get_data(filename=None):
+    data = []
     if filename and os.path.exists(os.path.join('./',filename)):
-        data=[]
+        with open(os.path.join('./',filename)) as json_file:
+            try:
+                data = json.load(json_file)
+            except json.decoder.JSONDecodeError:
+                print(f"Invalid JSON in {filename}")
     else:
-        print(f'File {filename} not found. Using {EXAMPLE_JSON_FILE} instead.')
-        if not os.path.exists(os.path.join('./',EXAMPLE_JSON_FILE)):
-            pass #write file from DEFAULT_JSON
+        if filename:
+            print(f'File {filename} not found. Using {EXAMPLE_JSON_FILE} instead.')
+        if os.path.exists(os.path.join('./',EXAMPLE_JSON_FILE)):
+            with open(os.path.join('./',EXAMPLE_JSON_FILE)) as json_file:
+                try:
+                    data = json.load(json_file)
+                except json.decoder.JSONDecodeError:
+                    print(f"Invalid JSON in {EXAMPLE_JSON_FILE}")
+        else:
+            with open(os.path.join('./',EXAMPLE_JSON_FILE), 'w') as json_file:
+                json_file.write(json.dumps(DEFAULT_JSON))
+            data=DEFAULT_JSON
     return data
 
 
 if __name__ == "__main__":
-    data = get_data()
-    birthday_list = get_birthday_list(data)
-    if birthday_list:
-        print(birthday_list)
-    else:
-        print('There are no birthdays today')
+    import argparse
+    cli_parser = argparse.ArgumentParser()
+    cli_parser.add_argument("--file", "-f", type=str, required=False)
+    filename = cli_parser.parse_args().file
+
+    data = get_data(filename)
+    if data:
+        birthday_list = get_birthday_list(data)
+        if birthday_list:
+            print(birthday_list)
+        else:
+            print('There are no birthdays today')
